6644180eca2ce819bd5486f59f30917ebbe2b011
Patryk <patrykdziedzioch@gmail.com>
2018-07-15 20:41:03 +0200

    Added login page from Spring and error verification

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -11,17 +11,27 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 @ComponentScan(basePackages="com.luv2code.springsecurity.demo")
 public class DemoAppConfig {
-	
-	//define a bean for ViewResolver
-	
+
+	// define a bean for ViewResolver
+
 	@Bean
 	public ViewResolver viewResolver() {
 		
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		
-		viewResolver.setPrefix("WEB-INF/view/");
+		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
+		
 		return viewResolver;
 	}
-
+	
 }
+
+
+
+
+
+
+
+
+
